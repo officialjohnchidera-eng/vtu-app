7e30afde8bd5aa0889f85b07bc4a710ebb5c20da
Chhidera <chipeter633@gmail.com>
2026-05-17 16:42:01 -0600

    Add Railway domain to ALLOWED_HOSTS

--- vtu_app/vtu_app/core/settings.py
+++ vtu_app/vtu_app/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vtu-app-production.up.railway.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
