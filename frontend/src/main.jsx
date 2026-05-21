6295a435aceedbca8c39dec9cb62936da23f6933
Chhidera <chipeter633@gmail.com>
2026-05-21 09:59:45 +0100

    changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 import './index.css'
 import App from './App.jsx'
-import '@tabler/icons-webfont/tabler-icons.min.css';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
